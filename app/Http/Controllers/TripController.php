c713ceb2c514b36b2b0ddf67bc185c08ba973365
Somunachimso Nzenwa <120026203+somunaexe@users.noreply.github.com>
2024-10-08 13:10:33 +0100

    trip functions implemented

--- app/Http/Controllers/TripController.php
+++ app/Http/Controllers/TripController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Models\Trip;
 class TripController extends Controller
 {
     public function store(Request $request)
@@ -19,5 +19,68 @@ class TripController extends Controller
             'destination',
             'destination_name'
         ]));
+    }
+
+    public function show(Request $request, Trip $trip)
+    {
+        if ($trip->user->id === $request->user()->id) {
+            return $trip;
+        }
+
+        if ($trip->driver && $request->user()->driver && $trip->driver->id === $request->user()->driver->id) {
+            return $trip;
+        }
+        return response()->json(['message' => 'Cannot find this trip.'], 404);
+    }
+
+    public function accept(Request $request, Trip $trip)
+    {
+        $request->validate([
+            'driver_location' => 'required'
+        ]);
+
+        $trip->update([
+            'driver_id' => $request->user()->id,
+            'driver_location' => $request->driver_location
+        ]);
+
+        $trip->load('driver.user');
+
+        return $trip;
+    }
+
+    public function start(Request $request, Trip $trip)
+    {
+        $trip->update([
+            'is_started' => true
+        ]);
+
+        $trip->load('driver.user');
+        return $trip;
+    }
+    
+    public function end(Request $request, Trip $trip)
+    {
+        $trip->update([
+            'is_complete' => true
+        ]);
+
+        $trip->load('driver.user');
+        return $trip;
+    }
+    
+    public function location(Request $request, Trip $trip)
+    {
+        $request->valdate([
+            'driver_location' => 'required'
+        ]);
+
+        $trip->update([
+            'driver_location' => $request->driver_location
+        ]);
+
+        $trip->load('driver.user');
+
+        return $trip;
     }
 }
